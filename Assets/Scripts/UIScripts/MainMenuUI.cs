714daa6db1ca8d084f6c6a89972eee9b73b0aba3
Raymond Brown <daimyoplays@gmail.com>
2024-12-29 19:35:27 -0600

    Main Menu initialized

--- Assets/Scripts/UIScripts/MainMenuUI.cs
+++ Assets/Scripts/UIScripts/MainMenuUI.cs
@@ -9,6 +9,7 @@ public class MainMenuUI : MonoBehaviour
     public Button controlsButton;
     public Button settingsButton;
     public Button quitButton;
+    public Button colorPalettesButton;
 
 
     private void Awake()
@@ -18,7 +19,48 @@ public class MainMenuUI : MonoBehaviour
 
     private void OnEnable()
     {
-        
+        sandboxModeButton = ui.Q<Button>("SandboxMode");
+        sandboxModeButton.clicked += OnSandboxModeButtonClicked;
+        regularModeButton = ui.Q<Button>("RegularMode");
+        regularModeButton.clicked += OnRegularModeButtonClicked;
+        controlsButton = ui.Q<Button>("Controls");
+        controlsButton.clicked += OnControlsButtonClicked;
+        settingsButton = ui.Q<Button>("Settings");
+        settingsButton.clicked += OnSettingsButtonClicked;
+        quitButton = ui.Q<Button>("Quit");
+        quitButton.clicked += OnQuitButtonClicked;
+        colorPalettesButton = ui.Q<Button>("ColorPalettes");
+        colorPalettesButton.clicked += OnColorPalettesButtonClicked;
+
     }
 
+    private void OnSandboxModeButtonClicked()
+    {
+        Debug.Log("Sandbox Mode!");
+    }
+
+    private void OnRegularModeButtonClicked()
+    {
+        Debug.Log("Regular Mode!");
+    }
+
+    private void OnControlsButtonClicked()
+    {
+        Debug.Log("Controls!");
+    }
+
+    private void OnSettingsButtonClicked()
+    {
+        Debug.Log("Settings!");
+    }
+
+    private void OnQuitButtonClicked()
+    {
+        Debug.Log("Quit!");
+    }
+
+    private void OnColorPalettesButtonClicked()
+    {
+        Debug.Log("Color Palettes!");
+    }
 }
